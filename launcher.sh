78ec6b14157bd42cffd55fb75e322b8c05ac9a8c
Simon Golpe <simon.golpe@snom.com>
2021-01-24 15:44:27 +0100

    Update path

--- launcher.sh
+++ launcher.sh
@@ -5,7 +5,7 @@ stty -F /dev/ttyAMA0 19200 cs8 parenb -parodd -icrnl -opost -isig -icanon -echo
 echo "serial ttyAMAO configured, this are the new settings:"
 stty -F /dev/ttyAMA0 -a
 echo "Starting KNX URL Gateway server..."
-/usr/local/gateway/build/urlGateway
+/usr/local/gateway/KnxHttpGateway
 echo "Starting KNX monitoring at file knxMonitor.log..."
 cat /dev/ttyAMA0 | od -tx1 >> knxMonitor.log
 
