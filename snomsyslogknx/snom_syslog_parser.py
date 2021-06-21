0ba4076a1c4faf5187472323c68c3f5366c7246a
simon.golpe <simon.golpe@snom.com>
2021-06-21 09:52:37 +0200

    use function in iot django app

--- snomsyslogknx/snom_syslog_parser.py
+++ snomsyslogknx/snom_syslog_parser.py
@@ -25,19 +25,6 @@ BOOTSTRAP = {
     'dark': 'alert-dark'
 }
 
-def get_local_ip():
-    local_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        local_socket.connect(('10.255.255.255', 1))
-        local_ip = local_socket.getsockname()[0]
-    except Exception:
-        local_ip = '127.0.0.1'
-    finally:
-        local_socket.close()
-    return local_ip
-
-
 def add_ip_client(ip_address):
     MAC = str(getmac.get_mac_address(ip=ip_address)).replace(":", '')
     TEMPLATE = (
