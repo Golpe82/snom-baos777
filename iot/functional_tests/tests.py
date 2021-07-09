6c4e24274bc8cc321afad9b7225e80ac7b21a9e6
Simon Golpe <simon.golpe@snom.com>
2021-07-09 14:11:46 +0200

    first steps of FT for layout + styling

--- iot/functional_tests/tests.py
+++ iot/functional_tests/tests.py
@@ -111,3 +111,29 @@ class NewVisitorTest(LiveServerTestCase):
         # # explanatory text to that effect.
         # self.fail('Finish the test!')
         # # She visits that URL - her to-do list is still there.
+
+    def test_layout_and_styling(self):
+        # Golpe goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # He notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # he starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
