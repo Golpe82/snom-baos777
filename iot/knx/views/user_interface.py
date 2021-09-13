ad0ec8e0ff9657d1c401d884a26c1fd021d05c6d
simon <simon.golpe@snom.com>
2021-09-13 11:48:58 +0200

    Remove unneeded view

--- iot/knx/views/user_interface.py
+++ iot/knx/views/user_interface.py
@@ -171,16 +171,3 @@ def knx_status(request):
     }
 
     return render(request, "knx/knx_status.html", context)
-
-def get_groupaddress_status(request, main, midd, sub):
-    request_address = f"{ main }/{ midd }/{ sub }"
-    status_object = KnxStatus.objects.get(groupaddress=request_address)
-
-    logging.info(f"Status of { status_object.groupaddress }: { status_object.status }")
-
-    data = {
-        "Groupaddress": status_object.groupaddress,
-        "Status": status_object.status
-    }
-
-    return JsonResponse(data)
