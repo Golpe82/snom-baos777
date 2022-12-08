3465a51f7dd43efeed86b8a68e2f69dc1c134b7c
simon.golpe <simon.golpe@snom.com>
2022-12-08 12:21:03 +0100

    change syslog clients

--- snomsyslogknx/syslog_clients.py
+++ snomsyslogknx/syslog_clients.py
@@ -1,10 +1,10 @@
 SYSLOG_CLIENTS = {
-    "192.168.178.20": {
-        "label": "Sofa lamp",
-        "min brightness": 170,
-        "max brightness": 200,
-        "switch groupaddress": "1/1/10",
-        "relative dim groupaddress": "1/1/11",
+    "10.110.16.59": {
+        "label": "Simóns desk",
+        "min brightness": 30,
+        "max brightness": 45,
+        "switch groupaddress": "1/1/20",
+        "relative dim groupaddress": "1/1/21",
     },
     # "10.110.16.102": {
     #     "label": "Visitor´s desk",
