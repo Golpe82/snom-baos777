fbbe51cf26676dc0e22db496a9060708ea51ce06
simon golpe <simongolpe@gmail.com>
2022-12-09 10:21:51 +0100

    refactor

---
+++ snomsyslogknx/syslog_actions.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import requests
+import logging
+
+class Hosts:
+    DEV_HOST = "10.110.16.101"
+    LOCALHOST = "localhost"
+
+POST_STATUS_URL = f"http://{Hosts.LOCALHOST}:8000/knx/values"
+DEV_POST_STATUS_URL = F"http://{Hosts.DEV_HOST}:8000/knx/values"
+# GET_RULES_URL = "http://localhost:8000/knx/rules/"
+KNX_URL = f"http://{Hosts.LOCALHOST}:1234/"
+DEV_GATEWAY_URL = f"http://{Hosts.DEV_HOST}:1234/"
+STATUS_URL = f"http://{Hosts.LOCALHOST}:8000/knx/status/"
+DEV_STATUS_URL = f"http://{Hosts.DEV_HOST}:8000/knx/status/"
+
+logging.basicConfig(level=logging.INFO)
+
+
+# TODO: REFACTOR
+
+class DPT3Values:
+    INCREASE = "-plus"
+    DECREASE = "-minus"
+
+
+class DBActions(object):
+    def als_save(self, client_ip, client_mac, raw_value, value):
+        data = {
+            "mac_address": client_mac,
+            "ip_address": client_ip,
+            "raw_value": raw_value,
+            "value": value,
+        },
+        try:
+            requests.post(POST_STATUS_URL, data=data)
+        except requests.exceptions.ConnectionError:
+            logging.warning(
+                f"Default localhost not reachable, trying {DEV_POST_STATUS_URL}..."
+            )
+            requests.post(DEV_POST_STATUS_URL, data=data)
+        except Exception:
+            logging.exception(f"Could not post data {data}:\n")
+
+
+class KNXActions(object):
+    def __init__(self, client_info):
+        self.client_info = client_info
+        self.client_label = client_info.get("label")
+
+    def knx_dimm_relative(self, groupaddress, lux_value):
+        client_min_brightness = self.client_info.get("min brightness")
+        client_max_brightness = self.client_info.get("max brightness")
+
+        if lux_value < client_min_brightness:
+            self._knx_increase(groupaddress)
+        elif lux_value > client_max_brightness:
+            self._knx_decrease(groupaddress)
+
+    def _knx_increase(self, groupaddress):
+        value = DPT3Values.INCREASE
+        logging.info(f"{self.client_label} dimming up {groupaddress}")
+
+        try:
+            requests.get(f"{KNX_URL}{groupaddress}{value}")
+        except requests.exceptions.ConnectionError:
+            logging.warning(
+                f"Default localhost not reachable, trying {DEV_GATEWAY_URL}..."
+            )
+            requests.get(
+                f"{DEV_GATEWAY_URL}{groupaddress}{value}"
+            )
+        except Exception:
+            logging.exception(
+                f"Could not increase {groupaddress}."
+                "KNX gateway not reachable or invalid groupaddress/value:\n"
+            )
+
+    def _knx_decrease(self, groupaddress):
+        value = DPT3Values.DECREASE
+        logging.info(
+            f"{self.client_label} dimming down {groupaddress}"
+        )
+
+        try:
+            requests.get(f"{KNX_URL}{groupaddress}{value}")
+        except requests.exceptions.ConnectionError:
+            logging.warning(
+                f"Default localhost not reachable, trying {DEV_GATEWAY_URL}..."
+            )
+            requests.get(
+                f"{DEV_GATEWAY_URL}{groupaddress}{value}"
+            )
+        except Exception:
+            logging.exception(
+                f"Could not decrease {groupaddress}."
+                "KNX gateway not reachable or invalid groupaddress/value:\n"
+            )
+
+    def knx_send_celsius(self, groupaddress, celsius_value):
+        logging.info(
+            f"{self.client_label} sending {celsius_value}°C to {groupaddress}"
+        )
+        path = f"{groupaddress}-send_celsius={celsius_value}"
+
+        try:
+            url = f"{KNX_URL}{path}"
+            logging.info(f"Trying to send url {url}...")
+            requests.get(url)
+        except requests.exceptions.ConnectionError:
+            logging.warning(
+                f"Default localhost not reachable, trying {DEV_GATEWAY_URL}..."
+            )
+            url = f"{DEV_GATEWAY_URL}{path}"
+            logging.info(f"Trying to send url {url}...")
+            requests.get(url)
+        except Exception:
+            logging.exception(
+                f"Could not send {celsius_value}°C to {groupaddress}."
+                "KNX gateway not reachable or invalid groupaddress/value:\n"
+            )
+
+    def get_groupaddress_status(self, groupaddress):
+        try:
+            status = requests.get(f"{STATUS_URL}{groupaddress}/")
+
+        except requests.exceptions.ConnectionError:
+            logging.warning(
+                f"Default localhost not reachable, trying {DEV_GATEWAY_URL}..."
+            )
+            status = requests.get(f"{DEV_STATUS_URL}{groupaddress}/")
+
+        except Exception:
+            logging.exception(f"No status for groupaddress {groupaddress}")
+
+        return status.json().get("Status")
--- snomsyslogknx/syslog_udp_handler.py
+++ snomsyslogknx/syslog_udp_handler.py
@@ -3,7 +3,7 @@ import socketserver
 
 import getmac
 
-from snom_syslog_parser import KNXActions, DBActions, to_lux
+from syslog_actions import KNXActions, DBActions
 from syslog_clients import SYSLOG_CLIENTS
 
 logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filemode='a')
@@ -21,26 +21,6 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
         self.temp_value = None
         logging.info(self.client_mac)
 
-    def handle(self):
-        for message_item in self.message_data:
-            if "ALS_VALUE" in message_item:
-                ambient_light = message_item.split(":")
-                self.als_value =  int(ambient_light[1])
-                self.database_actions.als_save(self.client_ip, self.client_mac, self.als_value, self.lux_value)
-                logging.info(f"{self.client_info.get('label')}: {self.lux_value} lux")
-
-                if self.knx_action.get_status() == "on":
-                    self.knx_action.knx_dimm_relative(self.lux_value)
-                else:
-                    logging.warning("Not switched on")
-
-            elif message_item == "usb_temperature_event:":
-                temp_value_message = self.message_data[-1:]
-                temp_value = temp_value_message[0]
-                self.temp_value = temp_value[:5]
-                logging.info(f"{self.temp_value[:5]}°C")
-                self.knx_action.knx_send_celsius(self.temp_value.split(".")[0])
-
     @property
     def message(self):
         raw_data = self.request[0].strip()
@@ -52,4 +32,29 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
 
     @property
     def lux_value(self):
-        return to_lux(self.als_value)
+        value = int(self.als_value) * 6.5 / 100
+
+        return round(value, 2)
+
+    def handle(self):
+        for message_item in self.message_data:
+            if "ALS_VALUE" in message_item:
+                switch_groupaddress = self.client_info.get("switch groupaddress")
+                relative_dim_groupaddress = self.client_info.get("relative dim groupaddress")
+                ambient_light = message_item.split(":")
+                self.als_value =  int(ambient_light[1])
+                self.database_actions.als_save(self.client_ip, self.client_mac, self.als_value, self.lux_value)
+                logging.info(f"{self.client_info.get('label')}: {self.lux_value} lux")
+
+                if self.knx_action.get_groupaddress_status(switch_groupaddress) == "on":
+                    self.knx_action.knx_dimm_relative(relative_dim_groupaddress, self.lux_value)
+                else:
+                    logging.warning("Not switched on")
+
+            elif message_item == "usb_temperature_event:":
+                send_celsius_groupaddress = self.client_info.get("send celsius gropuaddress")
+                temp_value_message = self.message_data[-1:]
+                temp_value = temp_value_message[0]
+                self.temp_value = temp_value[:5]
+                logging.info(f"{self.temp_value[:5]}°C")
+                self.knx_action.knx_send_celsius(send_celsius_groupaddress, self.temp_value.split(".")[0])
