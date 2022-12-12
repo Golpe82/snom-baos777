81e34a9af45ac8e9a91cee26d060f05ae011c709
simon golpe <simongolpe@gmail.com>
2022-12-12 16:32:51 +0100

    update clients

--- snomsyslogknx/syslog_clients.py
+++ snomsyslogknx/syslog_clients.py
@@ -1,19 +1,20 @@
 # TODO: this should be managed in django ui (save this in the portal db)
 
 SYSLOG_CLIENTS = {
-    "10.110.16.59": {
-        "label": "Simóns desk",
+    # "10.110.16.59": {
+    #     "label": "Simóns desk",
+    #     "min brightness": 30,
+    #     "max brightness": 45,
+    #     "switch groupaddress": "1/1/20",
+    #     "relative dim groupaddress": "1/1/21",
+    #     "send celsius gropuaddress": "1/6/1",
+    # },
+    "10.110.16.110": {
+        "label": "Visitor´s desk",
         "min brightness": 30,
         "max brightness": 45,
         "switch groupaddress": "1/1/20",
         "relative dim groupaddress": "1/1/21",
         "send celsius gropuaddress": "1/6/1",
-    },
-    # "10.110.16.102": {
-    #     "label": "Visitor´s desk",
-    #     "min brightness": 100,
-    #     "max brightness": 110,
-    #     "switch groupaddress": "1/1/20",
-    #     "relative dim groupaddress": "1/1/21",
-    # }
+    }
 }
