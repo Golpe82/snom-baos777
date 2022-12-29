434f197935fecbaaae310648e1c8e35c2750c275
simon golpe <simongolpe@gmail.com>
2022-12-29 19:47:27 +0100

    fix retreiveing status

--- snomsyslogknx/syslog_actions.py
+++ snomsyslogknx/syslog_actions.py
@@ -135,7 +135,4 @@ class KNXActions(object):
         except Exception:
             logging.exception(f"No status for groupaddress {groupaddress}")
 
-        logging.info(f"Url sent: {status.url}")
-        logging.info(status)
-        logging.info(status.json())
         return status.json().get("Status")
