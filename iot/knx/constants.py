a23e5360638e4a13957d28d313ef958816b5b98e
simon <simon.golpe@snom.com>
2023-03-23 22:50:47 +0100

    fix led range of d785

--- iot/knx/constants.py
+++ iot/knx/constants.py
@@ -17,18 +17,6 @@ class FkeyLEDNo(Enum):
     D713 = range(1, 5)
     D717 = range(5, 8)
     D735 = range(5, 13)
-    D785 = range(5, 11)
+    D785 = range(5, 29)
     D862 = range(5, 13)
     D865 = range(5, 15)
-
-# led_mapping = {}
-# for phone_model in FkeyLEDNo:
-#     print(phone_model.name)
-#     #phone_models.append(phone_model)
-#     leds = []
-#     for led in phone_model.value:
-#         print(led)
-#         leds.append(led)
-#     led_mapping[phone_model.name] = leds
-
-# print(led_mapping)
