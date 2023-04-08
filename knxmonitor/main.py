5d585e96dfb00b3015ce5a22812dc9401118909d
O.W <wittigoliver@googlemail.com>
2023-04-08 18:49:13 +0200

    BAOS 777 monitor

--- knxmonitor/main.py
+++ knxmonitor/main.py
@@ -15,6 +15,89 @@ STOPBYTE = b"\x16"
 
 logging.basicConfig(level=logging.DEBUG)
 
+''' BAOS 777 monitor '''
+BAOS_777 = True
+
+import websocket
+import _thread
+import time
+import rel
+
+import json
+import requests
+import time
+
+def get_token():
+    # log in to BAOS 777
+    # token is global for all http requests, not only websockets
+    done = False
+    while not done:
+        try:
+            r = requests.post('http://10.110.16.63/rest/login', json={"password": "admin", "username": "admin"})
+        
+
+            # if auth fails, we get 429 with e.g. {"login_blocked_for_s":24}'
+            if r.status_code == 200:
+                # token could be empty or too short 
+                if len(r.text) > 10:
+                    token = r.text  
+                    done = True
+                else:
+                    logging.critical(f"token={r.text} invalid?")
+            else:
+                logging.critical(f"Unable to authenticate BAOS 777 websocket with admin user!")
+                logging.critical(f"BAOS returned: {r.text}")
+                time.sleep(2)
+        except:
+            logging.critical(f"Unable to connect to BAOS 777!")
+            pass
+    logging.info(f"New BAOS token: {token}")
+    return token
+
+class BAOSMonitor:
+    def __init__(self):  
+        websocket.enableTrace(False)
+        self.token = get_token()
+        WS_HOST = 'ws://10.110.16.63/websocket'
+        URL = '{}?token={}'.format(WS_HOST, self.token)
+  
+        self.ws = websocket.WebSocketApp(URL,
+                                         on_open=self.on_open,
+                                         on_message=self.on_message,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close)
+        # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+        self.ws.run_forever(dispatcher=rel, reconnect=2)  
+        rel.signal(2, rel.abort)  # Keyboard Interrupt
+        rel.dispatch()
+        self.opened = False
+        
+    def on_message(self, ws, message):
+        print(F'Message:::{message}')
+
+        # message should be a json message already.
+        message = json.loads(message)
+        knx_monitor.DBActions.monitor_status_save_777(message)
+
+    def on_error(self, ws, error):
+        print(error)
+
+    def on_close(self, ws, close_status_code, close_msg):
+        print("### closed ###")
+        rel.abort()
+        print("### re-connect ###")
+        self.token = get_token()
+        self.ws.run_forever(dispatcher=rel)
+        rel.signal(2, rel.abort)
+        rel.dispatch()
+
+    def on_open(self, ws):
+        print("Opened connection")
+        time.sleep(2)
+    
+def main_777():
+    baos_777_ws = BAOSMonitor()
+    
 def main():
     with serial.Serial(DEVICE, BAUDRATE, CHARACTER_SIZE, PARITY) as connection:
         telegram_old = b"\xFF"
@@ -43,4 +126,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if BAOS_777:
+        main_777()
+    else:
+        main()
