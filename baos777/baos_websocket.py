6911a32f2cf2f4c156359ea3751ed633f08bb573
simon golpe <simongolpe@gmail.com>
2023-04-24 10:58:00 +0200

    move baos message class to dedicated module

--- baos777/baos_websocket.py
+++ baos777/baos_websocket.py
@@ -1,5 +1,6 @@
+"""It creates a BAOS websocket connection"""
+
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
 import logging
 import json
 from http import HTTPStatus
@@ -8,25 +9,21 @@ import requests
 import websocket
 
 from baos777.http_handler import HTTPHandler
-from baos777.baos_data import BAOS777Data
-from knxmonitor import knx_monitor
+from baos777.baos777_interface import BAOS777Interface
+from baos777.baos_indication_message import BAOSIndicationsMessage
+from baos777 import utils
 
 logging.basicConfig(level=logging.DEBUG)
 
 if logging.getLogger().level == logging.DEBUG:
     websocket.enableTrace(True)
 
-class BAOSIndicationsMessage:
-    def __init__(self, message):
-        self.message = message
-        indications = self.message.get("indications")
-        self.values = indications.get("values")
-
 
 class BaseWebsocket(ABC):
     def __init__(self):
         self.token = None
-        self.baos_data = None
+        self.baos_interface = None
+        self.incoming_message = None
 
     @abstractmethod
     def on_message(self, ws, message):
@@ -84,8 +81,8 @@ class BaseWebsocket(ABC):
             logging.exception("BAOS Webservice down, try reconnect")
 
         logging.info(f"Running websocket forever:\nId {id(self.ws)}\nToken: {self.token}\n")
-        self.baos_data = BAOS777Data(self.token)
-        logging.info(self.baos_data.sending_groupaddresses)
+        self.baos_interface = BAOS777Interface(self.token)
+        logging.info(self.baos_interface.sending_groupaddresses)
         # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
         self.ws.run_forever(ping_interval=60, ping_timeout=2, ping_payload="keep alive")
 
@@ -104,21 +101,19 @@ class BaseWebsocket(ABC):
 class MonitorWebsocket(BaseWebsocket):
     def on_message(self, ws, message):
         logging.info(f"BAOS event:\n{message}\n")
-        self.baos_data.baos_message = json.loads(message)
-        indication_message = BAOSIndicationsMessage(self.baos_data.baos_message)
-        led_update_urls = self.build_led_update_urls(indication_message)
-        self.send_urls(led_update_urls)
+        self.baos_interface.baos_message = json.loads(message)
+        led_update_urls = self._build_led_update_urls()
+        utils.send_urls(led_update_urls)
 
-    def build_led_update_urls(self, indication_message):
+    def _build_led_update_urls(self):
         urls_to_send = []
         knx_gateway = "10.110.16.59:8000"
         led_update_url = f"http://{knx_gateway}/knx/update_led_subscriptors/"
+        self.incoming_message = BAOSIndicationsMessage(self.baos_interface.baos_message)
 
-        for datapoint in indication_message.values:
-            datapoint_id = datapoint.get("id")
-            datapoint_sending_groupaddress = self.baos_data.get_sending_groupaddress(datapoint_id)
+        for datapoint_id, datapoint_value in self.incoming_message.values_by_datapoint_id.items():
+            datapoint_sending_groupaddress = self.baos_interface.get_sending_groupaddress(datapoint_id)
             led_update_groupaddress_url = f"{led_update_url}{datapoint_sending_groupaddress}/"
-            datapoint_value = datapoint.get("value")
 
             # TODO: make a class for mapping all possible values
             if datapoint_value == True:
@@ -131,24 +126,12 @@ class MonitorWebsocket(BaseWebsocket):
                 logging.error("Invalid value")
 
         return urls_to_send
-    
-    def send_urls(self, urls):
-        # when urls coming from phone must be checked if groupaddress is in baos sending groupaddress list.
-        # E.g. if phone http://10.110.16.59:1234/4/1/10-aus, check if 4/1/10 is a sending groupaddress in baos device
-        # otherwise request must be discarded or inform user "is not a sending address in baos device"
-        for url in urls:
-            try:
-                response = requests.get(url)
-
-                if response.status_code != HTTPStatus.OK:
-                    raise response.raise_for_status()
-            except Exception:
-                logging.error(f"exception sending {url}:")
-            
-            logging.info(f"Sent url {url}")
-
 
 
 class KNXWriteWebsocket(BaseWebsocket):
+    def on_message(self, ws, message):
+        ...
+
+class KNXReadWebsocket(BaseWebsocket):
     def on_message(self, ws, message):
         ...
--- baos777/utils.py
+++ baos777/utils.py
@@ -1,5 +1,9 @@
 import time
 import sys
+from http import HTTPStatus
+import logging
+
+import requests
 
 
 def wait(seconds):
@@ -10,3 +14,15 @@ def wait(seconds):
         time.sleep(1)
 
     sys.stdout.write("\n0 seconds remaining\n")
+
+def send_urls(urls: list) -> None:
+    for url in urls:
+        try:
+            response = requests.get(url)
+
+            if response.status_code != HTTPStatus.OK:
+                raise response.raise_for_status()
+        except Exception:
+            logging.error(f"exception sending {url}:")
+        
+        logging.info(f"Sent url {url}")
