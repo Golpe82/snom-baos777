d8f120783f2064e0708c651bb1a57225bbc20c18
simon golpe <simongolpe@gmail.com>
2023-04-25 08:43:06 +0200

    add baos777 constants file

---
+++ baos777/constants.py
@@ -0,0 +1,9 @@
+"""Module for BAOS 777 constants"""
+
+class BAOS777Commands:
+    NO_COMMAND = 0
+    SET_VALUE = 1
+    SEND_VALUE_ON_BUS = 2
+    SET_VALUE_AND_SEND_ON_BUS = 3
+    READ_VALUE_VIA_BUS = 4
+    CLEAR_DATAPOINT_TRANSMISSION_STATE = 5
