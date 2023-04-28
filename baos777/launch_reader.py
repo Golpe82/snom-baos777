7828b151f82261b8b9875b881256afe26c0fe4ee
simon golpe <simongolpe@gmail.com>
2023-04-28 09:35:09 +0200

    add reader launcher

---
+++ baos777/launch_reader.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+# add current folder to the system path
+sys.path.append("/usr/local/gateway")
+from baos777 import baos_websocket as baos_ws
+
+logging.basicConfig(level=logging.DEBUG)
+
+USERNAME = "admin"
+PASSWORD = "admin"
+
+if __name__ == "__main__":
+    reader = None
+    
+    while True:
+        if not reader:
+            logging.error("No token. Creating KNX reader...")
+            reader = baos_ws.KNXReadWebsocket(USERNAME, PASSWORD)
+            interface = reader.baos_interface
+            logging.info(f"Datapoints information:\n{interface.datapoints_information}")
+            logging.info(f"\nAvailable groupaddresses to read:\n{interface.sending_groupaddresses}")
