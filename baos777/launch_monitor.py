3ce360341e47a6a87bebd0be648587f7e0b15544
simon golpe <simongolpe@gmail.com>
2023-04-28 14:38:31 +0200

    undo change

--- baos777/launch_monitor.py
+++ baos777/launch_monitor.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 # add current folder to the system path
-sys.path.append(".")
+sys.path.append("/usr/local/gateway")
 from baos777 import baos_websocket as baos_ws
 
 logging.basicConfig(level=logging.DEBUG)
