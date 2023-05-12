31f02000012813ec8a10530ce99d05e3586597da
Simón Golpe <simon.golpe@snom.com>
2023-05-12 14:09:24 +0200

    add status icons to mb

--- iot/iot/urls.py
+++ iot/iot/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -17,3 +18,5 @@ urlpatterns = [
     path("snom_sensors/", include(snom_sensors_urls, namespace="snom_sensors")),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
