57460557387c107b3332a74e61c4f30771ab12f2
simon <simon.golpe@snom.com>
2023-05-22 10:36:25 +0200

    update unit files

--- snomiot.conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "System rebooted, KNX launcher running..."
-stty -F /dev/ttyAMA0 19200 cs8 parenb -parodd -icrnl -opost -isig -icanon -echo
-
-echo "serial ttyAMAO configured, this are the new settings:"
-stty -F /dev/ttyAMA0 -a
