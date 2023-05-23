ec7e3ac9cd7e54ebd7edbb31b5ae268f966b2a9a
Simón Golpe <simon.golpe@snom.com>
2023-05-23 15:24:02 +0200

    fix import

--- iot/knx/views/minibrowser.py
+++ iot/knx/views/minibrowser.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from knx.models import Groupaddress
 import baos777.baos_websocket as baos_ws
 
 BAOS_USERNAME = "admin"
