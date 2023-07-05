faff5f9e011d92560dd7a6a0da24344f48c392f8
simon golpe <simongolpe@gmail.com>
2023-07-05 18:56:35 +0200

    implement blink url

---
+++ iot/knx/views/blink.py
@@ -0,0 +1,32 @@
+import logging
+import argparse
+import time
+import baos777.baos_websocket as baos_ws
+
+parser = argparse.ArgumentParser(description='Blink KNX groupaddress (DPT1)')
+parser.add_argument('groupaddress', metavar='GA', type=str,
+                    help='KNX groupaddress to blink')
+parser.add_argument('seconds_for_on', metavar='on (sec)', type=int,
+                    help='time in seconds for on status')
+parser.add_argument('seconds_for_off', metavar='off (sec)', type=int,
+                    help='time in seconds for off status')
+parser.add_argument('user', metavar='username', type=str,
+                    help='Username for BAOS 777')
+parser.add_argument('passwd', metavar='password', type=str,
+                    help='Password for BAOS 777')
+
+args = parser.parse_args()
+
+
+logging.info(f"Starting to blink groupaddress {args.groupaddress}")
+
+while True:
+    try:
+        writer = baos_ws.KNXWriteWebsocket(args.user, args.passwd)
+    except Exception:
+        logging.error("not able to create a KNX write object")
+    else:
+        writer.baos_interface.send_value(args.groupaddress, "on")
+        time.sleep(args.seconds_for_on)
+        writer.baos_interface.send_value(args.groupaddress, "off")
+        time.sleep(args.seconds_for_off)
