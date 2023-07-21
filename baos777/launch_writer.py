862591c6f17dd4937d96c01ded3fec3aa122a5fe
simon golpe <simongolpe@gmail.com>
2023-07-21 10:51:50 +0200

    move knx folder to snom folder to make db writable

--- baos777/launch_writer.py
+++ baos777/launch_writer.py
@@ -1,9 +1,5 @@
 import logging
-import sys
 
-# add current folder to the system path
-# sys.path.append("/usr/local/gateway")
-sys.path.append("/knx")
 from baos777 import baos_websocket as baos_ws
 
 logging.basicConfig(level=logging.INFO)
